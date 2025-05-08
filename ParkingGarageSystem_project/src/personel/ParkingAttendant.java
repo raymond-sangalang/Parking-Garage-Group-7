0054c398af7383fcce8a11d3b4e5f883bb275bc5
raymond-sangalang <59466702+raymond-sangalang@users.noreply.github.com>
2025-05-07 19:50:57 -0700

    Update ParkingAttendant.java
    omit unnecessary comments and removed previously used username and password

--- ParkingGarageSystem_project/src/personel/ParkingAttendant.java
+++ ParkingGarageSystem_project/src/personel/ParkingAttendant.java
@@ -1,15 +1,16 @@
 package personel;
 import java.io.Serializable;
 
+import exception.ParkingExceptions;
 import modules.Payment;
 import modules.Ticket;
 
 public class ParkingAttendant extends User implements Serializable { // implements Employee, Serializable {
-    protected String parkingAttendantID;
+   
+	protected String parkingAttendantID;
     protected static int count = 0;
     protected String name;
-//    protected String username;
-    protected String password;
+
 
     public ParkingAttendant(String name, String username, String password) {
         super(username, password);
@@ -24,6 +25,7 @@ public class ParkingAttendant extends User implements Serializable { // implemen
         System.out.println("This ticket (" + ticket.getTicketID() + ") was created manually.");
     }
 
+    
     public void handlePayment(Ticket ticket) {
         if (ticket == null) {
             System.out.println("No ticket provided for payment.");
@@ -33,47 +35,34 @@ public class ParkingAttendant extends User implements Serializable { // implemen
             System.out.println("Ticket " + ticket.getTicketID() + " cannot be paid. Current status: " + ticket.getParkingTicketStatus());
             return;
         }
-        Payment payment = new Payment(ticket);
-        payment.processPayment(Payment.getCardPaymentStatus());
-
-        System.out.println("Payment successful for Ticket " + ticket.getTicketID() + ". Amount Paid: $" + payment.getAmount() + " via " + payment.getPaymentMethod());
+        Payment payment;
+		try {
+			payment = new Payment(ticket);     
+			payment.processPayment(Payment.getCardPaymentStatus());
+			System.out.println("Payment successful for Ticket " + ticket.getTicketID() + ". Amount Paid: $" + payment.getAmount() + " via " + payment.getPaymentMethod());
+  
+		} catch (ParkingExceptions e) {
+			e.printStackTrace();
+		}
+   
     }
 
 
     public boolean login(String username, String password) {
         return this.username.equals(username) && this.password.equals(password);
     }
-
     public String getParkingAttendantID() {
         return parkingAttendantID;
     }
-
-//    public void setParkingAttendantID(String employeeID) {
-//        this.parkingAttendantID = employeeID;
-//    }
-
+    public String getUsername() {
+    	return super.getUsername();
+    }
     public String getName() {
         return name;
     }
-
+    
     public void setName(String name) {
         this.name = name;
     }
-
-    public String getUsername() {
-        return username;
-    }
-
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
 
 }
