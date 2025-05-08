94e3a6cdbb1156cd3b5eec9fc23f86e17b3f2fcb
raymond-sangalang <59466702+raymond-sangalang@users.noreply.github.com>
2025-05-07 19:23:39 -0700

    Update LevelDisplayBoard.java
    documentations added

--- ParkingGarageSystem_project/src/modules/LevelDisplayBoard.java
+++ ParkingGarageSystem_project/src/modules/LevelDisplayBoard.java
@@ -4,17 +4,20 @@ import java.io.Serializable;
 
 public class LevelDisplayBoard implements Serializable  {
 	/*
-	 	
+	 	class LevelDisplayBoard -
+	 	   hardware sensor within a given floor.
+	 	   Useful for within the system, enabling clients omit searching 
+	 	   a parking level for an available parking space
 	 */
 	private int freeSpaces;
 	
 	public LevelDisplayBoard(int freeSpaces) {
 		this.setFreeSpaces(freeSpaces);
 	}
+	
 	public void displayNumFreeSpaces() {
 		System.out.println((freeSpaces == 0) ? "Parking Level Full" : String.format("Available Spaces: %d", freeSpaces));
 	}
-	
 	
 	public int getFreeSpaces() {
 		return freeSpaces;
@@ -25,9 +28,11 @@ public class LevelDisplayBoard implements Serializable  {
 	}
 	
 	public void occupySpace() {
+		// occupySpace: decrement the number of free spaces in level
 		this.setFreeSpaces(freeSpaces-1);
 	}
 	public void leaveSpace() {
+		// leaveSpace: increment the number of free spaces in level
 		this.setFreeSpaces(freeSpaces+1);
 	}
 
